666fe8ba7bb26424275524f7e531c07ae7098795
nowhere78 <wide1002@gmail.com>
2026-05-25 19:54:40 +0900

    feat: add PWA support for PWABuilder

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'food-roulette-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './style.css',
+  './icon.svg',
+  './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&family=Noto+Sans+KR:wght@300;400;700;900&display=swap'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
